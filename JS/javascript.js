ae02d2d0ab13cb6ce3bf107ead5c590dd574cfc4
Santiago Samaniego <bryanbssi@hotmail.com>
2015-10-28 08:22:52 -0500

    Funciones
    Uso de funciones Confirm y Prompt

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,7 +2,40 @@ alert("Hola mundo!");
 
 
 var num=1;
+
+
 while(num<=5){
         console.log(num);
         num++;
-            }
+            
+}
+
+
+for(num=5;num>=0;num--){
+console.log(num);
+}
+
+//Indicar si un numero es par o impar
+for(num=0;num<=5;num++){
+if(num%2==0){
+console.log("El numero "+num+" es par");
+}
+
+else{console.log("El numero "+num+" es impar");}
+
+}
+
+
+for(num=0;num<=5;num++){
+if(num==2||num==4){
+console.log("El numero ingresado es 2 o 4");
+}
+
+}
+
+//confirm("Excelente primera página con Javascript");
+
+//var respUsuario = confirm();
+//respUsuario
+
+var edad = prompt("Cuál es tu edad?");
